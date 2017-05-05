ecce2f3e5c303750afc7d9a91c772398ddab799b
Kristof Torfs <kristof@torfs.org>
2017-05-05 16:42:39 +0200

    Implemented "key query" API method

--- src/AppBundle/Controller/KeysController.php
+++ src/AppBundle/Controller/KeysController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 class KeysController extends ApiController
 {
+    protected function getAuthorizedKeysFilename() {
+        $user = posix_getpwuid(posix_getuid());
+        return sprintf('%s/.ssh/authorized_keys', $user['dir']);
+    }
+
     /**
      * @Route("/keys", name="keys", methods={"POST"})
      */
@@ -19,11 +24,29 @@ class KeysController extends ApiController
         $key = $request->get('key');
 
         $append = sprintf("\n# Nanobox key %s\n%s", $name, $key);
-        $user = posix_getpwuid(posix_getuid());
-        $filename = sprintf('%s/.ssh/authorized_keys', $user['dir']);
+        $filename = $this->getAuthorizedKeysFilename();
         @mkdir(dirname($filename), 0700, true);
         file_put_contents($filename, $append, FILE_APPEND);
 
         return $this->json(['id' => $name], 201);
+    }
+
+    /**
+     * @Route("/keys/{id}", name="keys-query", methods={"GET"})
+     * @param string $id
+     */
+    public function queryAction(string $id) {
+        if (!$this->verifyAccessToken()) {
+            return $this->throwError('Access was denied');
+        }
+        $keys = file_get_contents($this->getAuthorizedKeysFilename());
+        $key = '';
+        $ret = preg_match_all('/^# Nanobox key (.*)\n(.*)$/m', $keys, $matches, PREG_SET_ORDER);
+        if ($ret > 0) {
+            foreach($matches as $match) {
+                if ($match[1] == $id) $key = $match[2];
+            }
+        }
+        return $this->json(['id' => $id, 'name' => $id, 'key' => $key], 201);
     }
 }
