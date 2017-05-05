1c1662021cb978aa35aeb8a6a67fd7ff61267f85
Kristof Torfs <kristof@torfs.org>
2017-05-05 15:59:17 +0200

    Initial project creation

---
+++ README.md
@@ -0,0 +1,4 @@
+nanobox-adapter-single-server
+=============================
+
+A Symfony project created on April 29, 2017, 6:58 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends ApiController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
