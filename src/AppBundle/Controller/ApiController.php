bdfd6512483c113c84d38370fec3379ebfc0b8c5
Kristof Torfs <kristof@torfs.org>
2017-05-05 16:00:42 +0200

    Created base controller

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+abstract class ApiController extends Controller
+{
+    protected function getJson($basename) {
+        $path = $this->container->get('kernel')->locateResource('@AppBundle') . '/Json/' . $basename;
+        if (!file_exists($path)) {
+            throw new \InvalidArgumentException('The json file could not be located.');
+        }
+        $json = json_decode(file_get_contents($path));
+        if (is_null($json)) {
+            throw new \InvalidArgumentException('The json file is invalid.');
+        }
+        return $json;
+    }
+
+    protected function verifyAccessToken() {
+        $wanted = $this->container->getParameter('access_token');
+        $given = $this->container->get('request_stack')->getCurrentRequest()->headers->get('Auth-Access-Token');
+        return $given == $wanted;
+    }
+}
