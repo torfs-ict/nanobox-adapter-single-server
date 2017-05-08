34f0fba48125d185ae022826367c622eccad94b9
Kristof Torfs <kristof@torfs.org>
2017-05-08 20:27:10 +0200

    Add the SSH key when ordering the server

--- src/AppBundle/Controller/ServersController.php
+++ src/AppBundle/Controller/ServersController.php
@@ -3,18 +3,34 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ServersController extends ApiController
 {
+    protected function getAuthorizedKeysFilename() {
+        $user = posix_getpwuid(posix_getuid());
+        $filename = sprintf('%s/.ssh/authorized_keys', $user['dir']);
+        if (!file_exists($filename)) {
+            @mkdir(dirname($filename), 0700, true);
+            touch($filename);
+        }
+        return $filename;
+    }
+
     /**
      * @Route("/servers", name="servers", methods={"POST"})
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         if (!$this->verifyAccessToken()) {
             return $this->throwError('Access was denied');
+        }
+        $key = $request->get('ssh_key');
+        if (!empty($key)) {
+            $append = sprintf("\n%s", $key);
+            file_put_contents($this->getAuthorizedKeysFilename(), $append, FILE_APPEND);
         }
         return $this->json($this->getJson('servers.json'), 201);
     }
